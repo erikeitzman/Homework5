23b90fc7120f7e53b79506e0cf0208865688a884
Ayush Manish Agrawal <ayush.agrawal7661@gmail.com>
2015-11-07 14:17:50 -0600

    d

--- ProjectName/src/JDBCStuff/ConnectionFactory.java
+++ ProjectName/src/JDBCStuff/ConnectionFactory.java
@@ -1,6 +1,6 @@
 package JDBCStuff;
 
-
+import org.apache.log4j.Logger;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -8,9 +8,9 @@ import java.sql.SQLException;
 public class ConnectionFactory {
     //static reference to itself
     private static ConnectionFactory instance = new ConnectionFactory();
-    public static final String URL = "jdbc:mysql://localhost/jdbcdb";
-    public static final String USER = "YOUR_DATABASE_USERNAME";
-    public static final String PASSWORD = " YOUR_DATABASE_PASSWORD";
+    public static final String URL = "jdbc:mysql://cse.unl.edu:3306/eeitzman";
+    public static final String USER = "eeitzman";
+    public static final String PASSWORD = "letmein";
     public static final String DRIVER_CLASS = "com.mysql.jdbc.Driver"; 
      
     //private constructor
@@ -27,7 +27,13 @@ public class ConnectionFactory {
         try {
             connection = DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (SQLException e) {
-            System.out.println("ERROR: Unable to Connect to Database.");
+        	  static Logger log = Logger.getLogger(ConnectionFactory.class.getName());
+        	   
+        	   public static void main(String[] args)throws IOException,SQLException{
+        	      log.debug("Unable to connect to Database");
+        	      
+        	   }
+        	}
         }
         return connection;
     }   
