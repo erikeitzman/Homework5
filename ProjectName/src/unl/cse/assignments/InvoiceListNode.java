c82e29ff08adec7d9c3a2d2d4be592bdaebbdea8
Erik <Erik@10.214.51.29>
2015-11-23 19:17:39 -0600

    fadsfadsfasdfadsfadf

--- ProjectName/src/unl/cse/assignments/InvoiceListNode.java
+++ ProjectName/src/unl/cse/assignments/InvoiceListNode.java
@@ -1,5 +1,7 @@
 package unl.cse.assignments;
 
+import java.util.Arrays;
+
 import com.airamerica.Invoice;
 
 public class InvoiceListNode {
@@ -7,6 +9,23 @@ public class InvoiceListNode {
     private InvoiceListNode next;
     private Invoice item;
 
+    public static Invoice [] insertionSort(Invoice list[]) {
+		Invoice result[] = Arrays.copyOf(list, list.length);
+		int j;                    
+		Invoice key;                
+		int i;  
+
+		for (j = 1; j < result.length; j++){
+			key = result[j];
+			for(i = j - 1; (i >= 0) && (result[ i ].compareStrStr(key)>= 0); i--)
+			{
+				result[ i+1 ] = result[ i ];
+			}
+			result[ i+1 ] = key;
+		}
+		return result;
+	}
+    
     public InvoiceListNode(Invoice item, InvoiceListNode next) {
         this.item = item;
         this.next = null;
--- ProjectName/src/unl/cse/assignments/LinkedList.java
+++ ProjectName/src/unl/cse/assignments/LinkedList.java
@@ -41,6 +41,12 @@ public class LinkedList {
 		return size;
 	}
 	
+	public void emptyList(){
+		start = null;
+		end = null;
+		size = 0;
+	}
+	
 	public void insertAtStart(Invoice val){
 		InvoiceListNode aNode = new InvoiceListNode(val, null);
 		size++;
@@ -50,6 +56,132 @@ public class LinkedList {
 		}else{
 			aNode.setNext(start);
 			start = aNode;
+		}
+	}
+	
+	public void insertSortByCustomer(Invoice val){
+		InvoiceListNode aNode = new InvoiceListNode(val, null);
+		size++;
+		if(start == null){
+			start = aNode;
+			end = start;
+			return;
+		}
+		InvoiceListNode temp1 = start;
+		InvoiceListNode temp2 = start.getNext();
+		if(size == 2){
+			if(val.compareStrStr(temp1.getinvoice())==1){
+				insertAtStart(val);
+				return;
+			}else{
+				start.setNext(aNode);
+				end = aNode;
+				return;
+			}
+		}
+		if(val.compareStrStr(temp1.getinvoice())==1){
+			insertAtStart(val);
+			return;
+		}
+		for(int i = 0; i<size-1; i++){
+			if((val.compareStrStr(temp1.getinvoice())==1 && val.compareStrStr(temp2.getinvoice())==-1) || val.compareStrStr(temp1.getinvoice())==0){
+				aNode.setNext(temp2);
+				temp1.setNext(aNode);
+				return;
+			}else{
+				temp1 = temp2;
+				temp2 = temp2.getNext();
+				if (temp2 == null){
+					end = aNode;
+					temp1.setNext(aNode);
+					return;
+				}
+			}
+		}
+	}
+	
+	public void insertSortBySub(Invoice val){
+		InvoiceListNode aNode = new InvoiceListNode(val, null);
+		size++;
+		if(start == null){
+			start = aNode;
+			end = start;
+			return;
+		}
+		InvoiceListNode temp1 = start;
+		InvoiceListNode temp2 = start.getNext();
+		if(size == 2){
+			if(val.compareDoubDoub(temp1.getinvoice())==1){
+				insertAtStart(val);
+				return;
+			}else{
+				start.setNext(aNode);
+				end = aNode;
+				return;
+			}
+		}
+		if(val.compareDoubDoub(temp1.getinvoice())==1){
+			insertAtStart(val);
+			return;
+		}
+		for(int i = 0; i<size-1; i++){
+			if((val.compareDoubDoub(temp1.getinvoice())==-1 && val.compareDoubDoub(temp2.getinvoice())==1) || val.compareDoubDoub(temp1.getinvoice())==0){
+				aNode.setNext(temp2);
+				temp1.setNext(aNode);
+				return;
+			}else{
+				temp1 = temp2;
+				temp2 = temp2.getNext();
+				if (temp2 == null){
+					end = aNode;
+					temp1.setNext(aNode);
+					return;
+				}
+			}
+		}
+	}
+	
+	public void insertSortByStuff(Invoice val){
+		InvoiceListNode aNode = new InvoiceListNode(val, null);
+		size++;
+		if(start == null){
+			start = aNode;
+			end = start;
+			return;
+		}
+		InvoiceListNode temp1 = start;
+		InvoiceListNode temp2 = start.getNext();
+		if(size == 2){
+			if(val.compareStrStr2(temp1.getinvoice())==1){
+				insertAtStart(val);
+				return;
+			}else{
+				start.setNext(aNode);
+				end = aNode;
+				return;
+			}
+		}
+		if(val.compareStrStr2(temp1.getinvoice())==1){
+			insertAtStart(val);
+			return;
+		}
+		for(int i = 0; i<size-1; i++){
+			if(val.compareStrStr2(temp1.getinvoice())==1 && val.compareStrStr2(temp2.getinvoice())==-1){
+				aNode.setNext(temp2);
+				temp1.setNext(aNode);
+				return;
+			}else if(val.compareStrStr2(temp1.getinvoice())==0){
+				
+			}
+			else{
+				temp1 = temp2;
+				temp2 = temp2.getNext();
+				if (temp2 == null){
+					end = aNode;
+					temp1.setNext(aNode);
+					return;
+				}
+			}
 		}
 	}
 }
